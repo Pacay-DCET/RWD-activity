c89237472af05f731dfacd9871d613f0f9f9bbae
ChrisJohn Pacay <chrisjohnpacay1@gmail.com>
2025-02-12 21:59:36 -0800

    Create date.js

---
+++ date.js
@@ -0,0 +1,12 @@
+let val; 
+const today = new Date();
+const birthday = new Date('9-10-1981 11:25:00');
+val = birthday
+val = today.getMonth();
+val = birthday.getMonth();
+val = birthday.getSeconds();
+val = birthday.getMinutes();
+val = birthday.getHours();
+val = birthday.getFullYear();
+val = birthday.getTime();
+console.log(val);
