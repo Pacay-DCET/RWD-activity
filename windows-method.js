2ea6e749674d27b6d2d0ab6126c415ae30a119a6
ChrisJohn Pacay <chrisjohnpacay1@gmail.com>
2025-02-12 21:56:34 -0800

    windows_method.js

---
+++ windows-method.js
@@ -0,0 +1,9 @@
+val = window.location;
+val = window.location.hostname;
+val = window.location.port;
+val = window.location.href = 'http://google.com';
+val = window.location.search;
+val = window.location.pathname;
+//val = window.location.reload();
+val = window.history.go(-1);
+console.log(val)
