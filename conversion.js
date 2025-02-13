b35217fab0783a83a40077ad37d05c11e415a01a
ChrisJohn Pacay <chrisjohnpacay1@gmail.com>
2025-02-12 21:57:22 -0800

    Create conversion.js

---
+++ conversion.js
@@ -0,0 +1,15 @@
+val = String(5)
+
+val = String(4 + 4)
+
+val = String(true)
+
+val = String(new Date())
+
+val = String('5')
+
+val = String(true)
+
+val = String([1, 2, 3, 4])
+
+val = (5).toString()
