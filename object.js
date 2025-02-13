e76224f42e7531143372a01867478d25d24d41a4
ChrisJohn Pacay <chrisjohnpacay1@gmail.com>
2025-02-12 21:58:52 -0800

    Create object.js

---
+++ object.js
@@ -0,0 +1,12 @@
+const person = {
+    name: 'John',
+    age: 34,
+    hobbies: ['reading','games','coding'],
+    greet: function(){
+        console.log('Hello');
+    }
+}
+console.log(person.name);
+console.log(person.age);
+console.log(person.hobbies);
+console.log(person.hobbies[0]);
